6a278348404a55ec3a8d1cdf2d5ca9ad739aa133
Karl <ksjogren@gmail.com>
2020-10-26 22:41:30 -0700

    add enemy combatnats

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,17 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roberto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
   
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose. ");
@@ -67,4 +70,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
 }
 
-fight();
+for( var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
