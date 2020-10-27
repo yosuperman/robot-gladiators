8e2ad89fcc25600e608352db9211e726648c160a
Karl <ksjogren@gmail.com>
2020-10-26 23:18:59 -0700

    add rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,6 +70,14 @@ var fight = function(enemyName) {
   };
 
 for( var i = 0; i < enemyNames.length; i++){
+
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i +1));
+      }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
